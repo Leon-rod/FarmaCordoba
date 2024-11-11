0a54cf41f5fc67ee8f402897efbf9eaef06eba0c
Leon-rod <leonardoezequielr02@gmail.com>
2024-11-10 21:59:18 -0300

    Adicion de combo y logica para establecimientos en el login

--- CLIENTE/js/Login/login.js
+++ CLIENTE/js/Login/login.js
@@ -1,24 +1,37 @@
+async function CargarEstablecimientos(){
+    const response = await fetch("https://localhost:44379/api/Establecimiento");
+    const establecimientos = await response.json();
+    const $establecimientoSelect = document.getElementById("userEstablishment");
+    establecimientos.forEach(establecimiento => {
+        const option = document.createElement("option");
+        option.value = establecimiento.idEstablecimiento;
+        option.textContent = establecimiento.nombre;
+        $establecimientoSelect.appendChild(option);
+    });
+}
+
 async function Loguear(){
     $username = document.getElementById("username").value;
     $password = document.getElementById("password").value;
+    $establishment = document.getElementById("userEstablishment").value;
     let flag = false;
     try{
         const response = await fetch("https://localhost:44379/api/PersonalEstablecimiento");
         const data = await response.json();
         data.forEach(item => {
-            if($username == item.idPersonalNavigation.nroDoc && $password == item.idPersonalNavigation.psw){
+            if($username == item.idPersonalNavigation.nroDoc && $password == item.idPersonalNavigation.psw && $establishment == item.idEstablecimientoNavigation.idEstablecimiento){
                 localStorage.setItem("nombrePersonal", `${item.idPersonalNavigation.nombre} ${item.idPersonalNavigation.apellido}`);
                 localStorage.setItem("cargoPersonal", item.idCargoNavigation.cargo1);
+                localStorage.setItem("establecimientoPersonal", item.idEstablecimientoNavigation.idEstablecimiento);
                 window.location.href = "../Inicio/inicio.html";
                 flag = true;
             }
         });
         if(!flag){
-
             LimpiarFormulario();
         }
     } catch (error) {
-        console.log("Error al cargar empleados", error);
+        mostrarToast(error.message, "bg-danger");
     }
 }
 
@@ -30,6 +43,7 @@ function LimpiarFormulario(){
 
 document.addEventListener("DOMContentLoaded",()=>{
     document.getElementById("loginBtn").addEventListener("click", Loguear);
+    CargarEstablecimientos();
 })
 
 function mostrarToast(mensaje, color = 'bg-danger', duracion = 3000) {
