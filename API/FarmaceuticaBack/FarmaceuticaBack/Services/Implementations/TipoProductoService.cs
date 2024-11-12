d8a91cf5ab2959aacaef2b13e2865ebd89c22036
412061-ramiroibazeta <412061@tecnicatura.frc.utn.edu.ar>
2024-11-11 21:02:25 -0300

    TiposProducto controller y comienzo producto abmc

---
+++ API/FarmaceuticaBack/FarmaceuticaBack/Services/Implementations/TipoProductoService.cs
@@ -0,0 +1,25 @@
+﻿using FarmaceuticaBack.Data.Contracts;
+using FarmaceuticaBack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmaceuticaBack.Services.Implementations
+{
+    public class TipoProductoService
+    {
+        private readonly ITipoProductoRepository _repository;
+
+        public TipoProductoService(ITipoProductoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<TiposProducto>> GetAll()
+        {
+            return await _repository.GetAll();
+        }
+    }
+}
