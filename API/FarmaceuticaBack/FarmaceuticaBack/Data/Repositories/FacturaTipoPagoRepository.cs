c02331bc92e16be4aa704d7ed0a05b79818a8890
Leon-rod <leonardoezequielr02@gmail.com>
2024-11-11 22:26:06 -0300

    adicion endpoint para el post de los metodos de pago de una factura

---
+++ API/FarmaceuticaBack/FarmaceuticaBack/Data/Repositories/FacturaTipoPagoRepository.cs
@@ -0,0 +1,26 @@
+﻿using FarmaceuticaBack.Data.Contracts;
+using FarmaceuticaBack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmaceuticaBack.Data.Repositories
+{
+    public class FacturaTipoPagoRepository : IFacturaTipoPagoRepository
+    {
+        private readonly FarmaceuticaContext _context;
+        public FacturaTipoPagoRepository(FarmaceuticaContext context)
+        {
+            this._context = context;
+        }
+        public async Task<bool> Insert(FacturasTiposPago facturasTiposPago)
+        {
+            bool result = false;
+            await _context.FacturasTiposPagos.AddAsync(facturasTiposPago);
+            result = await _context.SaveChangesAsync() > 0;
+            return result;
+        }
+    }
+}
