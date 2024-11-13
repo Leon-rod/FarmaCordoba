50daf9de14608ce5da8d22793e82c2c3a3830dbe
Franco Catania <francocatania93@gmail.com>
2024-11-13 00:26:09 -0300

    Se termina rolesNav.js y se agrega contraseña a todos los personales

--- CLIENTE/js/Utils/rolesNav.js
+++ CLIENTE/js/Utils/rolesNav.js
@@ -10,6 +10,143 @@ function CargarNav(){
     $userName.innerText = name;
     $userRol.innerText = rol;
     $navBar.innerHTML = '';
+    if(rol === "Gerente de Logística"){
+        const li = document.createElement("li");
+        li.innerHTML = `<li class="nav-item"><a class="nav-link active" aria-current="page" href="../Inicio/inicio.html">Inicio</a></li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Pedidos</a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="../Pedidos/pedidos.html">Ver pedidos</a></li>
+                                <li><a class="dropdown-item" href="../Pedidos/cargarPedido.html">Crear pedido</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Soporte</a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+  
+                                <li><a class="dropdown-item" href="../Stock/stock.html">Stock</a></li>
+
+                            </ul>
+                        </li>
+
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="../Acerca_de/acerca.html">Acerca de</a></li>`;
+        $navBar.appendChild(li);
+    }
+
+    if(rol === "Especialista en Farmacoeconomía"){
+        const li = document.createElement("li");
+        li.innerHTML = `    <li class="nav-item"><a class="nav-link active" aria-current="page" href="../Inicio/inicio.html">Inicio</a></li>
+                            <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dashboard</a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="../Dashboard/reporteOS.html">Reporte mensual Obra Social</a></li>
+                                <li><a class="dropdown-item" href="../Dashboard/totalesFarmacia.html">Totales facturados por Farmacia</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="../Acerca_de/acerca.html">Acerca de</a></li>`;
+        $navBar.appendChild(li);
+    }
+
+
+    if(rol === "Supervisor de Calidad"){
+        const li = document.createElement("li");
+        li.innerHTML = `<li class="nav-item"><a class="nav-link active" aria-current="page" href="../Inicio/inicio.html">Inicio</a></li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Soporte</a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="../Medicamentos/medicamentos.html">Medicamentos</a></li>
+                                <li><a class="dropdown-item" href="../Productos/producto.html">Productos</a></li>
+                                <li><a class="dropdown-item" href="../Stock/stock.html">Stock</a></li>
+                                <li><a class="dropdown-item" href="../Lotes/lotes.html">Lotes</a></li>
+
+                            </ul>
+                        </li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="../Acerca_de/acerca.html">Acerca de</a></li>`;
+        $navBar.appendChild(li);
+    }
+
+
+    if(rol === "Supervisor de Ventas"){
+        const li = document.createElement("li");
+        li.innerHTML = `<li class="nav-item"><a class="nav-link active" aria-current="page" href="../Inicio/inicio.html">Inicio</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Facturas</a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="../Factura/factura.html">Ver facturas</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dashboard</a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="../Dashboard/totalesFacturados.html">Totales facturados por vendedor</a></li>
+                            </ul>
+                        </li>
+    
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="../Acerca_de/acerca.html">Acerca de</a></li>`;
+        $navBar.appendChild(li);
+    }
+
+
+    if(rol === "Asistente de Ventas"){
+        const li = document.createElement("li");
+        li.innerHTML = `<li class="nav-item"><a class="nav-link active" aria-current="page" href="../Inicio/inicio.html">Inicio</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Facturas</a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="../Factura/factura.html">Ver facturas</a></li>
+                                <li><a class="dropdown-item" href="../Factura/cargarFactura.html">Crear factura</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Soporte</a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="../Medicamentos/medicamentos.html">Medicamentos</a></li>
+                                <li><a class="dropdown-item" href="../Productos/producto.html">Productos</a></li>
+                                <li><a class="dropdown-item" href="../Stock/stock.html">Stock</a></li>
+                                <li><a class="dropdown-item" href="../Lotes/lotes.html">Lotes</a></li>
+                            </ul>
+                        </li>
+    
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="../Acerca_de/acerca.html">Acerca de</a></li>`;
+        $navBar.appendChild(li);
+    }
+
+
+    if(rol === "Gerente de ventas"){
+        const li = document.createElement("li");
+        li.innerHTML = `<li class="nav-item"><a class="nav-link active" aria-current="page" href="../Inicio/inicio.html">Inicio</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Facturas</a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="../Factura/factura.html">Ver facturas</a></li>
+                                <li><a class="dropdown-item" href="../Factura/cargarFactura.html">Crear factura</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Soporte</a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="../Medicamentos/medicamentos.html">Medicamentos</a></li>
+                                <li><a class="dropdown-item" href="../Productos/producto.html">Productos</a></li>
+                                <li><a class="dropdown-item" href="../Stock/stock.html">Stock</a></li>
+                                <li><a class="dropdown-item" href="../Lotes/lotes.html">Lotes</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dashboard</a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="../Dashboard/totalesFacturados.html">Totales facturados por vendedor</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="../Acerca_de/acerca.html">Acerca de</a></li>`;
+        $navBar.appendChild(li);
+
+    }
+
+
     if(rol === "Asistente Administrativo"){
         const li = document.createElement("li");
         li.innerHTML = `    <li class="nav-item"><a class="nav-link active" aria-current="page" href="../Inicio/inicio.html">Inicio</a></li>
